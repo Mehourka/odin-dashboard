4474ec05dbfbf99634f6f20700574726d5601208
Karim <k.mehour@gmail.com>
2024-12-03 15:45:47 -0500

    Add main section content and styling

--- js/script.js
+++ js/script.js
@@ -10,12 +10,83 @@ function populate_profile_info(profile_info) {
 	handle_nodes.forEach((e) => {
 		e.textContent = "@" + profile_info.handle;
 	})
-
 }
+
+
+function populate_projects(projects) {
+	const container = document.getElementById("projects-container")
+	projects.forEach( (e) => {
+		const new_card = document.createElement("div");
+		const classes = ["project-card", "section-content"]
+		new_card.classList = classes.join(" ");
+
+		new_card.innerHTML = `
+			<h1>${e.title}</h1>
+			<p>${e.description}</p>
+		`
+
+
+		container.appendChild(new_card);
+	})
+}
+
+
 
 const profile_info = {
 	username: "Morgan Oakley",
 	handle: "morgan"
 }
 
+const projects = [
+	{
+		title: "Super Cool Project",
+		description: "Super cool project's descritpion with suff an things",
+	},
+	{
+		title: "Less cool Project",
+		description: "SOme description of a lesser project, dont in forgotten times",
+	},
+	{
+		title: "Unicorn App",
+		description: "Marvelous, yet more microservices than users ATM",
+	},
+	{
+		title: "Super Cool Project",
+		description: "Super cool project's descritpion with suff an things",
+	},
+	{
+		title: "Less cool Project",
+		description: "SOme description of a lesser project, dont in forgotten times",
+	},
+	{
+		title: "Unicorn App",
+		description: "Marvelous, yet more microservices than users ATM",
+	},
+	{
+		title: "Super Cool Project",
+		description: "Super cool project's descritpion with suff an things",
+	},
+	{
+		title: "Less cool Project",
+		description: "SOme description of a lesser project, dont in forgotten times",
+	},
+	{
+		title: "Unicorn App",
+		description: "Marvelous, yet more microservices than users ATM",
+	},
+	{
+		title: "Super Cool Project",
+		description: "Super cool project's descritpion with suff an things",
+	},
+	{
+		title: "Less cool Project",
+		description: "SOme description of a lesser project, dont in forgotten times",
+	},
+	{
+		title: "Unicorn App",
+		description: "Marvelous, yet more microservices than users ATM",
+	},
+]
+
 populate_profile_info(profile_info);
+populate_projects(projects);
